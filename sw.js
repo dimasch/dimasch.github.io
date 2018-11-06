dee242ee1749dc08e722149b3992b78f3e4e7d4d
Dmitry Schegolikhin <d.schegolikhin@kt-team.de>
2018-11-06 22:40:10 +0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-352d7acb6fa13ea72fe9.js"
+    "url": "webpack-runtime-94454fc012cb2ef77480.js"
   },
   {
     "url": "app-7393dfd090d6d97b7c5a.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0806ed6c4a8b7e0a417d9ac0f335a7fc"
+    "revision": "200d8fa15d705e10d90829638752ed42"
   },
   {
     "url": "component---src-pages-404-js-fe6dc2fafd22b034c202.js"
