f549f866ae605718c2a697c7a27e5198c3673b00
Dmitry Schegolihin <d.shegolihin@gmail.com>
2019-01-24 14:59:35 +0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f3117d8db1937b0a8a9b.js"
+    "url": "webpack-runtime-5e0fd0892f40c1dc75d6.js"
   },
   {
     "url": "app.237e04afc4258147e434.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "957c8ab1f1daeb8c2f46d2d0b022ba8a"
+    "revision": "728070ed7850db824f8498f308e9d2b5"
   },
   {
     "url": "component---src-pages-404-js-64142abe778ca170e5d3.js"
